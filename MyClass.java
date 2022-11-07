8a80d1fadb2edd7fcc31fcb79446287e0311a066
hussamalnees99 <hussamalnees@gmail.com>
2022-11-07 20:26:19 +0300

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -2,5 +2,8 @@ import java.io.*;
 class MyClass{
   public static void main(String args[]){
     System.out.println("This is the main function");
+    a =4;
+    b=5;
+    System.out.println(a+b);
   }
 }
