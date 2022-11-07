940016204b913b1501c9f8e2ca218cb363aa1bd1
hussamalnees99 <hussamalnees@gmail.com>
2022-11-07 20:21:46 +0300

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,6 @@
 import java.io.*;
 class MyClass{
   public static void main(String args[]){
-    System.out.println("This is the main function");
+    System.out.println("Pull Request Example");
   }
 }
